cbdc235b3bfea61c4664ed44454488c83250a8f7
trishacapitle <trishacapitle01@gmail.com>
2025-10-04 05:37:50 +0800

    fix: remove lost ui

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,9 @@
 import { Hero7 } from "@/components/hero";
 import { Pricing2 } from "@/components/pricing";
-import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div>
-			This is the landing page.
-			<Link href="/login" prefetch={false}>
-				Sign In
-			</Link>
+		<div className="flex flex-col items-center">
 			<Hero7 />
 			<Pricing2 />
 		</div>
