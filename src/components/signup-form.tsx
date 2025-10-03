f77c0b03f7db571b4b75c53b1c4f2923778bb5bf
trishacapitle <trishacapitle01@gmail.com>
2025-10-04 05:37:59 +0800

    feat: link to login

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -15,6 +15,7 @@ import {
   FieldSeparator
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import Link from "next/link"
 
 export function SignupForm({
   className,
@@ -97,7 +98,7 @@ export function SignupForm({
 							<Field>
 								<Button type="submit">Create Account</Button>
 								<FieldDescription className="text-center">
-									Already have an account? <a href="#">Sign in</a>
+									Already have an account? <Link href="/login">Sign in</Link>
 								</FieldDescription>
 							</Field>
 						</FieldGroup>
