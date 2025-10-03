42f368ab375a7cab9d60ad0eb105461ed5f1b7c8
trishacapitle <trishacapitle01@gmail.com>
2025-10-04 05:00:52 +0800

    feat: link to login

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			This is the landing page.
-			<Button>Sign In</Button>
+			<Link href="/login" prefetch={false}>
+				Sign In
+			</Link>
 		</div>
 	);
 }
